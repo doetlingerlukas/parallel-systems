8cd553bb5546a2eb9dcf2687d26291e2bf3f4585
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2019-10-16 23:51:18 +0200

    Add '--map-by' argument to scripts.

--- assignment01/latency.sh
+++ assignment01/latency.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
+while [[ $# -gt 0 ]]
+do 
+  key="$1"
+  case $key in 
+    -mb|--map-by)
+      mapping="--map-by $2"
+      shift
+      shift
+      ;;
+  esac
+done
+
 #$ -q std.q
 #$ -cwd
 #$ -N osu_latency
-#$ -o results/latency.dat
+#$ -o latency.dat
 #$ -j yes
 
 module load openmpi/4.0.1
 
 if [ $(whoami) = 'cb761016' ]
 then
-  mpiexec -display-allocation -display-map /home/cb76/cb761016/a_01/t2/micro-benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency
+  mpiexec $mapping -display-allocation -display-map /home/cb76/cb761016/a_01/t2/micro-benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency
 elif [ $(whoami) = 'cb761022' ]
 then
-  mpiexec -display-allocation -display-map /home/cb76/cb761022/micro_benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency
+  mpiexec $mapping -display-allocation -display-map /home/cb76/cb761022/micro_benchmarks/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency
 else 
   # TODO: enter path to exe for third user
   whoami
