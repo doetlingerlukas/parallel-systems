843839619840aa61814b0fba3870c61ad4f27b34
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2019-10-21 11:51:44 +0200

    Only use one Makefile and job script.

---
+++ assignment02/exercise2/job.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+while [[ $# -gt 0 ]]
+do 
+  key="$1"
+  case $key in 
+    -n|--mpi-n)
+      processes="$2"
+      shift
+      shift
+      ;;
+    -p|--program)
+      program="$2"
+      shift
+      shift
+      ;;
+  esac
+done
+
+#$ -q std.q
+#$ -cwd
+#$ -N heat_stencil
+#$ -o heat.dat
+#$ -j yes
+
+output="heat.dat"
+
+if [ -f $output ] ; then
+    rm $output
+fi
+
+module load gcc/8.2.0
+module load openmpi/4.0.1
+
+mpiexec -n $processes --mca btl tcp,self $program
