abb36115d38192cb20669463b4c5a28379536493
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2019-10-25 12:24:30 +0200

    Fix seq 2d heat stencil.

--- assignment03/exercise1/heat_stencil_2D_seq.cpp
+++ assignment03/exercise1/heat_stencil_2D_seq.cpp
@@ -40,18 +40,19 @@ int main(int argc, char **argv) {
 
         auto t_current = A[i][j];
 
-        auto t_left = (i != 0) ? A[i - 1][j] : t_current;
-        auto t_right = (i != N - 1) ? A[i + 1][j] : t_current;
-        auto t_upper = (j != 0) ? A[i][j - 1] : t_current;
-        auto t_lower = (j != N) ? A[i][j + 1] : t_current;
+        auto t_upper = (i != 0) ? A[i - 1][j] : t_current;
+        auto t_lower = (i != N - 1) ? A[i + 1][j] : t_current;
+        auto t_left = (j != 0) ? A[i][j - 1] : t_current;
+        auto t_right = (j != N - 1) ? A[i][j + 1] : t_current;
 
-        B[i][j] = t_current + 0.2 * (t_left + t_right + t_upper + t_lower - 4 * t_current);
+        B[i][j] = t_current + 0.2 * (t_left + t_right + t_upper + t_lower + (-4 * t_current));
       }
     }
     if (!(t % 1000)) {
       cout << "Step t= " << t << endl;
       printTemperature(A, N);
       cout << endl << endl;
+      cout << A[10][10] << " " << A[50][50] << endl;
     }
 
     // swap matrices (just pointers, not content)
@@ -68,37 +69,54 @@ void printTemperature(vector<vector<double>> m, int N) {
 
   // boundaries for temperature (for simplicity hard-coded)
   const double max = 273 + 30;
-  const double min = 0;
+  const double min = 273 + 0;
 
   // set the 'render' resolution
   int W = RESOLUTION;
+  int H = RESOLUTION;
   if (N < RESOLUTION){
     W = N;
+    H = N;
   }
 
 
   // step size in each dimension
   int sW = N / W;
+  int sH = N / H;
  
-  cout << "|";
+  // top wall
+  for (auto i = 0; i < W + 2; i++) {
+    cout << "-";
+  }
+  cout << endl;
   
-  for (auto i = 0; i < N; i++){
+  for (auto i = 0; i < H; i++){
+    // left wall
+    cout << "|";
+    
+    // computing a row
     for (auto j = 0; j < W; j++) {
       
       double max_t = 0;
-      for (auto x = sW * j; x < sW * j + sW; x++) {
-        max_t = (max_t < m[i][j]) ? m[i][j] : max_t;
+      for (auto x = sH * i; x < sH * i + sH; x++) {
+        for (auto y = sW * j; y < sW * j + sW; y++) {
+          max_t = (max_t < m[x][y]) ? m[x][y] : max_t;
+        }
       }
       double temp = max_t;
 
       // pick the 'color'
       int c = ((temp - min) / (max - min)) * numColors;
       c = (c >= numColors) ? numColors - 1 : ((c < 0) ? 0 : c);
-
       cout << colors[c];
     }
-    cout << endl;
+    // right wall
+    cout << "|" << endl;
   }
 
-  cout << "|";
+  // bottom wall
+  for (auto i = 0; i < W + 2; i++) {
+    cout << "-";
+  }
+  cout << endl;
 }
