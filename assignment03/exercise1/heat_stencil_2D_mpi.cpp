3cfb06805eef369bb7f6b20e2e4727b7c0d859d5
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2019-10-28 22:21:53 +0100

    Refactor 2d heat stencil.

--- assignment03/exercise1/heat_stencil_2D_mpi.cpp
+++ assignment03/exercise1/heat_stencil_2D_mpi.cpp
@@ -23,208 +23,186 @@ int main(int argc, char **argv) {
   if (argc > 1) {
     N = strtol(argv[1], nullptr, 10);
   }
-  auto T = N * 100;
-  cout << "Computing heat-distribution for room size N=" << N << "*"<< N << " for T=" << T << " timesteps." << endl;
+  auto timesteps = N * 100;
+  cout << "Computing heat-distribution for room size N=" << N << "*"<< N << " for T=" << timesteps << " timesteps." << endl;
 
-  int rank_id, number_ranks; 
+  int rank_id, amount_of_ranks; 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank_id);
-  MPI_Comm_size(MPI_COMM_WORLD, &number_ranks);
+  MPI_Comm_size(MPI_COMM_WORLD, &amount_of_ranks);
 
-  if (fmod(sqrtf(number_ranks), 1) != 0) {
+  if (fmod(sqrtf(amount_of_ranks), 1) != 0) {
     MPI_Finalize();
     return EXIT_FAILURE;
   }
-  int ranks_per_row = sqrt(number_ranks);
+  int ranks_per_row = sqrt(amount_of_ranks);
 
-  // set up grid
-  int dims[2] = {ranks_per_row, ranks_per_row};
-	int periods[2] = {0, 0}; // to avoid too many edge cases in code
-  //int mycoords[2];
+  // set up cartesian topology
+  int dimensions[2] = {ranks_per_row, ranks_per_row};
+	int periods[2] = {0, 0};
   MPI_Comm comm_2d;
-  MPI_Dims_create(number_ranks, 2, dims); 
-  MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &comm_2d);
+  MPI_Dims_create(amount_of_ranks, 2, dimensions); 
+  MPI_Cart_create(MPI_COMM_WORLD, 2, dimensions, periods, 0, &comm_2d);
 
-  // define neighbors
+  // Neighbours rank id's.
 	int left, right, upper, lower;
 	MPI_Cart_shift(comm_2d, 0, 1, &left, &right);
 	MPI_Cart_shift(comm_2d, 1, 1, &upper, &lower);
-
   cout << "rank: " << rank_id << "| left: " << left << "| right: " << right << "| upper: " << upper << "| lower: " << lower << endl;
 
-  // split problem size among ranks
-  auto N_rank = N / ranks_per_row;
+  // Problem size for a single rank.
+  auto chunk_size = N / ranks_per_row;
 
-  // init rank buffer
-  vector<vector<double>> buffer(N_rank);
-  vector<vector<double>> swap_buffer(N_rank);
-  for (auto i = 0; i < N_rank; i++){
-		buffer[i].resize(N_rank, 273);
-    swap_buffer[i].resize(N_rank, 273);
+  // Buffers for computation.
+  vector<vector<double>> buffer(chunk_size);
+  vector<vector<double>> swap_buffer(chunk_size);
+  for (auto i = 0; i < chunk_size; i++){
+		buffer[i].resize(chunk_size, 273);
+    swap_buffer[i].resize(chunk_size, 273);
   }
 
-  // source
+  // Place heat source.
   int source = N / 4;
-  int source_coords[2] = {(source / N_rank), (source / N_rank)};
+  int source_coords[2] = {(source / chunk_size), (source / chunk_size)};
   int source_rank;
   MPI_Cart_rank(comm_2d, source_coords, &source_rank);
-  auto source_index = source % N_rank;
-
+  auto source_index = source % chunk_size;
+  
   if (rank_id == source_rank) {
     buffer[source_index][source_index] = 273 + 60;
     cout << rank_id << " " << source_index << endl;
   }
 
 
-  // ---------- compute -------------
-  vector<double> upper_buffer(N_rank);
-  vector<double> lower_buffer(N_rank);
+  // Buffer to save upper/lower bordering row.
+  vector<double> upper_buffer(chunk_size);
+  vector<double> lower_buffer(chunk_size);
 
-  for (auto t = 0; t < T; t++) { 
+  // Iterate over timesteps.
+  for (auto t = 0; t < timesteps; t++) { 
 
     // send to lower
     if (lower >= 0) {
       MPI_Request req;
-      MPI_Isend(&buffer[N_rank-1][0], N_rank, MPI_DOUBLE, lower, TO_LOWER, comm_2d, &req);
+      MPI_Isend(&buffer[chunk_size-1][0], chunk_size, MPI_DOUBLE, lower, TO_LOWER, comm_2d, &req);
       MPI_Request_free(&req);
     }
     // send to upper and recieve from upper
     if (upper >= 0) {
       MPI_Request req;
-      MPI_Isend(&buffer[0][0], N_rank, MPI_DOUBLE, upper, TO_UPPER, comm_2d, &req);
+      MPI_Isend(&buffer[0][0], chunk_size, MPI_DOUBLE, upper, TO_UPPER, comm_2d, &req);
       MPI_Request_free(&req);
 
-      MPI_Recv(&upper_buffer[0], N_rank, MPI_DOUBLE, upper, TO_LOWER, comm_2d, MPI_STATUS_IGNORE);
+      MPI_Recv(&upper_buffer[0], chunk_size, MPI_DOUBLE, upper, TO_LOWER, comm_2d, MPI_STATUS_IGNORE);
     }
-
     // recieve from lower
     if (lower >= 0) {
-      MPI_Recv(&lower_buffer[0], N_rank, MPI_DOUBLE, lower, TO_UPPER, comm_2d, MPI_STATUS_IGNORE);
+      MPI_Recv(&lower_buffer[0], chunk_size, MPI_DOUBLE, lower, TO_UPPER, comm_2d, MPI_STATUS_IGNORE);
     }
 
     // iterate over rows
-    for (auto i = 0; i < N_rank; i++) { 
+    for (auto row = 0; row < chunk_size; row++) { 
 
       // send first element of current row to left neighbour
       if (left >= 0) {
         MPI_Request req;
-        MPI_Isend(&buffer[i][0], 1, MPI_DOUBLE, left, TO_LEFT, comm_2d, &req);
+        MPI_Isend(&buffer[row][0], 1, MPI_DOUBLE, left, TO_LEFT, comm_2d, &req);
         MPI_Request_free(&req);
       }
       // send last element of current row to right neigbour
       if (right >= 0) {
         MPI_Request req;
-        MPI_Isend(&buffer[i][N_rank - 1], 1, MPI_DOUBLE, right, TO_RIGHT, comm_2d, &req);
+        MPI_Isend(&buffer[row][chunk_size - 1], 1, MPI_DOUBLE, right, TO_RIGHT, comm_2d, &req);
         MPI_Request_free(&req);
       }
 
       // iterate over columns
-      for (auto j = 0; j < N_rank; j++) {
+      for (auto column = 0; column < chunk_size; column++) {
 
-        if (rank_id == source_rank && (i == source_index && j == source_index)) {
-            swap_buffer[i][j] = buffer[i][j];
+        if (rank_id == source_rank && (row == source_index && column == source_index)) {
+            swap_buffer[row][column] = buffer[row][column];
             continue;
         }
         
-        auto t_current = buffer[i][j];
+        auto current_temp = buffer[row][column];
         
-        auto t_left = (j != 0) ? buffer[i][j - 1] : t_current;
-        auto t_right = (j != N_rank - 1) ? buffer[i][j + 1] : t_current;
+        auto left_temp = (column != 0) ? buffer[row][column - 1] : current_temp;
+        auto right_temp = (column != chunk_size - 1) ? buffer[row][column + 1] : current_temp;
+        auto upper_temp = (row != 0) ? buffer[row - 1][column] : current_temp;
+        auto lower_temp = (row != chunk_size - 1) ? buffer[row + 1][column] : current_temp;
 
-        // recieve from left
-        if ((left >= 0) && (j == 0)) {
-          MPI_Recv(&t_left, 1, MPI_DOUBLE, left, TO_RIGHT, comm_2d, MPI_STATUS_IGNORE);
-        }
-        // recieve from right
-        if ((right >= 0) && (j == N_rank - 1)) { 
-          MPI_Recv(&t_right, 1, MPI_DOUBLE, right, TO_LEFT, comm_2d, MPI_STATUS_IGNORE);
+        if ((left >= 0) && (column == 0)) {
+          MPI_Recv(&left_temp, 1, MPI_DOUBLE, left, TO_RIGHT, comm_2d, MPI_STATUS_IGNORE);
         }
 
-        auto t_upper = (i != 0) ? buffer[i - 1][j] : t_current;
-        if ((upper >= 0) && (i == 0)) {
-          t_upper = upper_buffer[j];
+        if ((right >= 0) && (column == chunk_size - 1)) { 
+          MPI_Recv(&right_temp, 1, MPI_DOUBLE, right, TO_LEFT, comm_2d, MPI_STATUS_IGNORE);
         }
 
-        auto t_lower = (i != N_rank - 1) ? buffer[i + 1][j] : t_current;
-        if ((lower >= 0) && (i == N_rank - 1)) {
-          t_lower = lower_buffer[j];
+        if ((upper >= 0) && (row == 0)) {
+          upper_temp = upper_buffer[column];
         }
 
-        swap_buffer[i][j] = t_current + 0.2 * (t_left + t_right + t_upper + t_lower + (-4 * t_current));  
+        if ((lower >= 0) && (row == chunk_size - 1)) {
+          lower_temp = lower_buffer[column];
+        }
+
+        swap_buffer[row][column] = current_temp + 0.2 * (left_temp + right_temp + upper_temp + lower_temp + (-4 * current_temp));  
       }
     }
     
     // swap matrices (just pointers, not content)
     swap(buffer, swap_buffer);
   }
-  /*
-  cout << "-----------------------------" << endl;
-  cout << "rank id " << rank_id << endl;
-  printTemperature(buffer, N_rank);
-  cout << "-----------------------------" << endl;
-  */
-  
-  MPI_Datatype rank_subarray;
-  int size[2]    = {N, N};
-  int subsize[2] = {N_rank, N_rank}; 
-  int start[2] = {0, 0}; 
-  MPI_Type_create_subarray(2, size, subsize, start, MPI_ORDER_C, MPI_DOUBLE, &rank_subarray);
-  MPI_Type_commit(&rank_subarray);
 
-  if (rank_id == 0){ // collect rank_buffers from all nodes
+  // Collect results.
+  if (rank_id == 0){
+    // Array to save coordinates.
+    int coord[2];
+
     vector<vector<double>> result(N);
     for (auto i = 0; i < N; i++){
 		  result[i].resize(N);
     }
-    //vector<double> flat_result(N*N);
-    for (auto i = 1; i < number_ranks; i++){
+    
+    // Add buffers from ranks to result.
+    for (auto i = 1; i < amount_of_ranks; i++){
       
-      //MPI_Recv(&flat_result[0] + N_rank * i, (N_rank * N_rank), MPI_DOUBLE, i, TO_MAIN, comm_2d, MPI_STATUS_IGNORE);
-      auto recieve_size = N_rank * N_rank;
-      vector<double> A(recieve_size, 1);
-      MPI_Recv(&A[0], recieve_size, MPI_DOUBLE, i, TO_MAIN, comm_2d, MPI_STATUS_IGNORE);
+      auto receive_size = chunk_size * chunk_size;
+      vector<double> A(receive_size, 1);
+      MPI_Recv(&A[0], receive_size, MPI_DOUBLE, i, TO_MAIN, comm_2d, MPI_STATUS_IGNORE);
       cout << A[0] << endl;
       
-      // TODO merge arrays of size N_rank*N_rank in one array of size N*N
-      int coord[2];
       MPI_Cart_coords(comm_2d, i, 2, coord);
 
-      int x_start = coord[0];
-      int y_start = coord[1];
-
-      for (auto i = 0; i < N_rank; i++) { // iterate rows
-        for (auto j = 0; j < N_rank; j++) { // iterate columns
-          result[i + y_start * N_rank][j + x_start * N_rank] = A[j + i * N_rank];
+      for (auto row = 0; row < chunk_size; row++) {
+        for (auto column = 0; column < chunk_size; column++) {
+          result[row + coord[1] * chunk_size][column + coord[0] * chunk_size] = A[column + row * chunk_size];
         }
       }
     }
-    //add rank 0 results
-    int coord[2];
+
+    // Add buffer of rank 0 to result.
     MPI_Cart_coords(comm_2d, 0, 2, coord);
 
-    int x_start = coord[0];
-    int y_start = coord[1];
-
-    for (auto i = 0; i < N_rank; i++) { // iterate rows
-      for (auto j = 0; j < N_rank; j++) { // iterate columns
-        result[i + y_start * N_rank][j + x_start * N_rank] = buffer[i][j];
+    for (auto row = 0; row < chunk_size; row++) {
+      for (auto column = 0; column < chunk_size; column++) {
+        result[row + coord[1] * chunk_size][column + coord[0] * chunk_size] = buffer[row][column];
       }
     }
 
     printTemperature(result, N);
   } else { 
-    // send rank_buffer to rank 0
-    auto send_size = N_rank * N_rank;
+    // Send buffer to rank 0.
+    auto send_size = chunk_size * chunk_size;
 
     vector<double> to_send;
-    for (auto i = 0; i < N_rank; i++) {
-      for (auto j = 0; j < N_rank; j++) {
-        to_send.push_back(buffer[i][j]);
+    for (auto row = 0; row < chunk_size; row++) {
+      for (auto column = 0; column < chunk_size; column++) {
+        to_send.push_back(buffer[row][column]);
       }
     }
-    
     MPI_Send(&to_send[0], send_size, MPI_DOUBLE, 0, TO_MAIN, comm_2d);
-    cout << "rank " << rank_id << " sended subarray" << endl;
   }
 
   MPI_Finalize();
