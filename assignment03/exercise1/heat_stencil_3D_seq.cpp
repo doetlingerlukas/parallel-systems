18b305786b1a539543a6572f934eddb1d86eccc5
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2019-11-02 11:42:21 +0100

    Fix sequential 3d stencil.

--- assignment03/exercise1/heat_stencil_3D_seq.cpp
+++ assignment03/exercise1/heat_stencil_3D_seq.cpp
@@ -2,76 +2,66 @@
 #include <vector>
 #include <chrono>
 
-#define RESOLUTION 80
-
 using namespace std;
 
-void printTemperature(vector<vector<vector<double>>> m, int N);
+void printTemperature(vector<vector<double>> m, int problem_size);
 
 int main(int argc, char **argv) {
 
   // problem size
-  auto N = 20;
+  auto problem_size = 100;
   if (argc > 1) {
-    N = strtol(argv[1], nullptr, 10);
+    problem_size = strtol(argv[1], nullptr, 10);
   }
-  auto T = N * 20;
-  cout << "Computing heat-distribution for room size N=" << N << "*"<< N << "*" << N << " for T=" << T << " timesteps." << endl;
+  auto timesteps = problem_size * 100;
+  cout << "Computing heat-distribution with problem size " << problem_size << "^3" << " for " << timesteps << " timesteps." << endl;
 
   auto start_time = chrono::high_resolution_clock::now();
 
   // init matrix of size N*N
-  vector<vector<vector<double>>> A(N);
-  vector<vector<vector<double>>> B(N);
-  for (auto i = 0; i < N; i++){
-      A[i].resize(N);
-      B[i].resize(N);
-      for (auto j = 0; j < N; j++){
-		    A[i][j].resize(N, 273);
-        B[i][j].resize(N, 273);
-      }
-  }
+  vector<vector<vector<double>>> buffer_a(problem_size, vector<vector<double>>(problem_size, vector<double>(problem_size, 273)));
+  vector<vector<vector<double>>> buffer_b(problem_size, vector<vector<double>>(problem_size, vector<double>(problem_size, 273)));
 
-  auto source_x = N/4;
-  auto source_y = N/4;
-  auto source_z = N/4;
-  A[source_x][source_y][source_z] = 273 + 60;
+  auto source = problem_size / 4;
+  buffer_a[source][source][source] = 273 + 60;
 
-  for (auto t = 0; t < T; t++) { // iterate timesteps
-    for (auto i = 0; i < N; i++) {
-      for (auto j = 0; j < N; j++) {
-          for (auto k = 0; k < N; k++){
-            if (i == source_x && j == source_y && k == source_z) {
-                B[i][j][k] = A[i][j][k];
-                continue;
-            }
+  for (auto t = 0; t < timesteps; t++) {
+    for (auto slice = 0; slice < problem_size; slice++) {
+      for (auto row = 0; row < problem_size; row++) {
+        for (auto column = 0; column < problem_size; column++){
 
-            auto t_current = A[i][j][k];
-
-            auto t_upper = (i != 0) ? A[i - 1][j][k] : t_current;
-            auto t_lower = (i != N - 1) ? A[i + 1][j][k] : t_current;
-            auto t_left = (j != 0) ? A[i][j - 1][k] : t_current;
-            auto t_right = (j != N - 1) ? A[i][j + 1][k] : t_current;
-            auto t_before = (k != 0) ? A[i][j][k - 1] : t_current;
-            auto t_behind = (k != N - 1) ? A[i][j][k + 1] : t_current;
-
-            auto temp = t_current + 0.2 * (t_left + t_right + t_upper + t_lower + t_before + t_behind - 6 * t_current);
-
-            B[i][j][k] = temp;
+          if ((slice == source) && (row == source) && (column == source)) {
+            buffer_b[slice][row][column] = buffer_a[slice][row][column];
+            continue;
           }
+
+          auto t_current = buffer_a[slice][row][column];
+          
+          auto t_upper = (row != 0) ? buffer_a[slice][row - 1][column] : t_current;
+          auto t_lower = (row != problem_size - 1) ? buffer_a[slice][row + 1][column] : t_current;
+          auto t_left = (column != 0) ? buffer_a[slice][row][column - 1] : t_current;
+          auto t_right = (column != problem_size - 1) ? buffer_a[slice][row][column + 1] : t_current;
+          auto t_before = (slice != 0) ? buffer_a[slice - 1][row][column] : t_current;
+          auto t_behind = (slice != problem_size - 1) ? buffer_a[slice + 1][row][column] : t_current;
+
+          auto temp = t_current + 0.14 * (t_left + t_right + t_upper + t_lower + t_before + t_behind + (-6 * t_current));
+
+          buffer_b[slice][row][column] = temp;
+        }
       }
-    }
-    if (!(t % 50)) {
-      cout << "Step t= " << t << endl;
-      printTemperature(A, N);
-      cout << endl << endl;
     }
 
     // swap matrices (just pointers, not content)
-    swap(A, B);
+    buffer_b.swap(buffer_a);
+
+    if (!(t % 2000)) {
+      cout << "Step t= " << t << endl;
+      printTemperature(buffer_a[source], problem_size);
+      cout << endl << endl;
+    }
   }
 
-  printTemperature(A, N);
+  printTemperature(buffer_a[source], problem_size);
 
   // Measure time.
   auto end_time = chrono::high_resolution_clock::now();
@@ -82,54 +72,40 @@ int main(int argc, char **argv) {
   return EXIT_SUCCESS;
 }
 
-void printTemperature(vector<vector<vector<double>>> m, int N) {
+void printTemperature(vector<vector<double>> m, int problem_size) {
   const char *colors = " .-:=+*^X#%@";
   const int numColors = 12;
+
+  // render resolution
+  const int res_width = 80;
+  const int res_height = 50;
 
   // boundaries for temperature (for simplicity hard-coded)
   const double max = 273 + 30;
   const double min = 273 + 0;
 
-  // set the 'render' resolution
-  int W = RESOLUTION;
-  int H = RESOLUTION;
-  int D = RESOLUTION;
-  if (N < RESOLUTION){
-    W = N;
-    H = N;
-    D = N;
-  }
-
-
-  // step size in each dimension
-  int sW = N / W;
-  int sH = N / H;
-  int sD = N / D;
-
-  auto fixedDepth = (D / sD) / 2;
+  int width_step = problem_size > res_width ? problem_size / res_width : problem_size;
+  int height_step = problem_size > res_height ? problem_size / res_height : problem_size;
  
   // top wall
-  for (auto i = 0; i < W + 2; i++) {
+  for (auto wall = 0; wall < res_width + 2; wall++) {
     cout << "-";
   }
   cout << endl;
   
-  for (auto i = 0; i < H; i++){
+  for (auto row = 0; row < res_height; row++){
     // left wall
     cout << "|";
     
-    // computing a row
-    for (auto j = 0; j < W; j++) {
-      
+    for (auto column = 0; column < res_width; column++) {
       double max_t = 0;
-      for (auto x = sH * i; x < sH * i + sH; x++) {
-        for (auto y = sW * j; y < sW * j + sW; y++) {
-          max_t = (max_t < m[x][y][fixedDepth]) ? m[x][y][fixedDepth] : max_t;
+      for (auto x = height_step * row; x < height_step * row + height_step; x++) {
+        for (auto y = width_step * column; y < width_step * column + width_step; y++) {
+          max_t = (max_t < m[x][y]) ? m[x][y] : max_t;
         }
       }
       double temp = max_t <= max ? max_t : max - 1;
 
-      // pick the 'color'
       int c = ((temp - min) / (max - min)) * numColors;
       c = (c >= numColors) ? numColors - 1 : ((c < 0) ? 0 : c);
       cout << colors[c];
@@ -139,7 +115,7 @@ void printTemperature(vector<vector<vector<double>>> m, int N) {
   }
 
   // bottom wall
-  for (auto i = 0; i < W + 2; i++) {
+  for (auto wall = 0; wall < res_width + 2; wall++) {
     cout << "-";
   }
   cout << endl;
