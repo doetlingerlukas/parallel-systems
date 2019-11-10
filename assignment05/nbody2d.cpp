bf5c45fca5b25f62991ed16e6c3d012e386a398c
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2019-11-10 09:37:47 +0100

    Change indentation from 4 to 2.

--- assignment05/nbody2d.cpp
+++ assignment05/nbody2d.cpp
@@ -14,78 +14,78 @@ constexpr double M = 1;
 double fRand(double min, double max);
 
 class Particle {
-    private:
+  private:
 
-        double px, py; // position
-        double vx, vy; // velocity
-        double fx, fy; // force
-        double m = M; // mass
+    double px, py; // position
+    double vx, vy; // velocity
+    double fx, fy; // force
+    double m = M; // mass
 
-    public:
+  public:
 
-        Particle(){}
+    Particle(){}
 
-        Particle(int Nx, int Ny){
-            px = fRand(0, Nx);
-            py = fRand(0, Ny);
-            vx = fRand(0, 1.0);
-            vy = fRand(0, 1.0);
-        }
+    Particle(int Nx, int Ny){
+      px = fRand(0, Nx);
+      py = fRand(0, Ny);
+      vx = fRand(0, 1.0);
+      vy = fRand(0, 1.0);
+    }
 
-        void printParticle(){
-            cout << "position: (" << px << ", " << py << ")" <<endl;;
-        }
+    void printParticle(){
+      cout << "position: (" << px << ", " << py << ")" <<endl;;
+    }
 
-        double getRadius(Particle b){
-            double dx = px - b.px;
-            double dy = py - b.py;
-            double r = sqrt(dx*dx + dy*dy);
-            return r;
-        }
+    double getRadius(Particle b){
+      double dx = px - b.px;
+      double dy = py - b.py;
+      double r = sqrt(dx*dx + dy*dy);
+      return r;
+    }
 
-        int getX(){
-            return (int) px;
-        }
+    int getX(){
+      return (int) px;
+    }
 
-        int getY(){
-            return (int) py;
-        }
+    int getY(){
+      return (int) py;
+    }
 
-        void calculateForce(Particle b){
-            double rad = getRadius(b);
-            assert(rad > 0.0);
-            double F = G * (m * b.m) / (rad * rad);
-            fx = F * (px - b.px) / rad;
-            fy = F * (py - b.py) / rad;
-        }
+    void calculateForce(Particle b){
+      double rad = getRadius(b);
+      assert(rad > 0.0);
+      double F = G * (m * b.m) / (rad * rad);
+      fx = F * (px - b.px) / rad;
+      fy = F * (py - b.py) / rad;
+    }
 
-        void update(int Nx, int Ny){
-            vx += fx / m;
-            vy += fy / m;
-            px += vx;
-            py += vy;
-            
-            // handle egde cases, flip velocity sign if on border
-            if (px < 0.0){
-                px = 0.0;
-                vx = -vx;
-            } else if (px > Nx-1){
-                px = Nx-1;
-                vx = -vx;
-            }
-            if (py < 0.0){
-                py = 0.0;
-                vy = -vy;
-            } else if (py > Ny-1){
-                py = Ny-1;
-                vy = -vy;
-            }
-        }
+    void update(int Nx, int Ny){
+      vx += fx / m;
+      vy += fy / m;
+      px += vx;
+      py += vy;
+      
+      // handle egde cases, flip velocity sign if on border
+      if (px < 0.0){
+        px = 0.0;
+        vx = -vx;
+      } else if (px > Nx-1){
+        px = Nx-1;
+        vx = -vx;
+      }
+      if (py < 0.0){
+        py = 0.0;
+        vy = -vy;
+      } else if (py > Ny-1){
+        py = Ny-1;
+        vy = -vy;
+      }
+    }
 
-        void resetForce(){
-            fx = 0.0;
-            fy = 0.0;
-        }
+    void resetForce(){
+      fx = 0.0;
+      fy = 0.0;
+    }
 
 };
 
@@ -94,79 +94,79 @@ void printParticleVector2D(vector<Particle> particles, int N, int Nx, int Ny);
 
 int main(){
 
-    //room size
-    int Nx = 20;
-    int Ny = 20;
+  //room size
+  int Nx = 20;
+  int Ny = 20;
 
-    //number of particles
-    int N = 10;
+  //number of particles
+  int N = 10;
 
-    int timesteps = N;
+  int timesteps = N;
 
-    srand(time(NULL));
+  srand(time(NULL));
 
-    vector<Particle> particles;
-    particles.reserve(N);
-    for (auto i = 0; i < N; i++){
-        particles.emplace_back(Nx, Ny);
-    }
+  vector<Particle> particles;
+  particles.reserve(N);
+  for (auto i = 0; i < N; i++){
+    particles.emplace_back(Nx, Ny);
+  }
 
-    for (auto t = 0; t < timesteps; t++){
-        
-        for (int i = 0; i < N; ++i) {
-            particles[i].resetForce();
-            for (int j = 0; j < N; ++j) {
-                if (i != j) {
-                    particles[i].calculateForce(particles[j]);
-                }
-            }
-            particles[i].update(Nx, Ny);
+  for (auto t = 0; t < timesteps; t++) {
+    
+    for (int i = 0; i < N; ++i) {
+      particles[i].resetForce();
+      for (int j = 0; j < N; ++j) {
+        if (i != j) {
+          particles[i].calculateForce(particles[j]);
         }
-
-        cout << "timestep :" << t << endl;
-
-        printParticleVector2D(particles, N, Nx, Ny);
-
-        // sleep to see movement happen
-        this_thread::sleep_for(std::chrono::milliseconds(500));
+      }
+      particles[i].update(Nx, Ny);
     }
+
+    cout << "timestep :" << t << endl;
+
+    printParticleVector2D(particles, N, Nx, Ny);
+
+    // sleep to see movement happen
+    this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
 
 }
 
 double fRand(double min, double max){
-    double f = ((double)rand()-1) / RAND_MAX;
-    return min + f * (max - min);
+  double f = ((double)rand()-1) / RAND_MAX;
+  return min + f * (max - min);
 }
 
 void printParticleVector(vector<Particle> particles){
-    for (size_t i = 0; i < particles.size(); i++){
-        particles[i].printParticle();
-    }
+  for (size_t i = 0; i < particles.size(); i++){
+    particles[i].printParticle();
+  }
 }
 
 void printParticleVector2D(vector<Particle> particles, int N, int Nx, int Ny){
-    vector<vector<char>> result2D(Nx, vector<char>(Ny, ' '));
-    for (auto i = 0; i < N; i++){
-        Particle temp = particles[i];
-        //cout << temp.getX() << ", " << temp.getY()<<endl;
-        result2D[temp.getX()][temp.getY()] = 'x';
-    }
+  vector<vector<char>> result2D(Nx, vector<char>(Ny, ' '));
+  for (auto i = 0; i < N; i++){
+    Particle temp = particles[i];
+    //cout << temp.getX() << ", " << temp.getY()<<endl;
+    result2D[temp.getX()][temp.getY()] = 'x';
+  }
 
-    for (auto i = 0; i <= Nx+1; i++){
-        cout << "-";
-    }
-    cout << endl;
+  for (auto i = 0; i <= Nx+1; i++){
+    cout << "-";
+  }
+  cout << endl;
 
-    for (auto i = 0; i < Nx; i++){
-        cout << "|";
-        for (auto j = 0; j < Ny; j++){
-            cout << result2D[i][j];
-        }
-        cout << "|" << endl;
+  for (auto i = 0; i < Nx; i++){
+    cout << "|";
+    for (auto j = 0; j < Ny; j++){
+      cout << result2D[i][j];
     }
+    cout << "|" << endl;
+  }
 
-    for (auto i = 0; i <= Nx+1; i++){
-        cout << "-";
-    }
-    cout << endl;
+  for (auto i = 0; i <= Nx+1; i++){
+    cout << "-";
+  }
+  cout << endl;
 }
