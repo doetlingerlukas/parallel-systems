f344447024a7c0a322ec245fce9bc3a531e8cec5
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2019-11-24 15:15:46 +0100

    Refactor nbody openmp.

--- assignment07/nbody2d_omp.cpp
+++ assignment07/nbody2d_omp.cpp
@@ -16,13 +16,16 @@ int main(int argc, char **argv){
 
   //number of particles
   int N = 1000;
+  bool print = false;
 
   if (argc > 1) {
     N = strtol(argv[1], nullptr, 10);
+    if (argc > 2) {
+      print = (argv[2] == "true");
+    }
   }
 
   int timesteps = 1000;
-
   srand(42);
 
   // initialize particles (randomly)
@@ -31,38 +34,31 @@ int main(int argc, char **argv){
   for (auto i = 0; i < N; i++){
     particles.emplace_back(Nx, Ny, 0);
   }
-
   vector<Particle> buffer = particles;
-
 
   for (auto t = 0; t < timesteps; t++) {
     // at each timestep, calculate the forces between each pair of particles
-    
-      int i;
-      #pragma omp parallel for private(i)
-      for (i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-          if(j < i){
-            buffer[i].updateForce(buffer[j]);
-          }
-          else if (i > j) {
-            buffer[i].calculateForce(particles[j]);
-          }
+    int i;
+    #pragma omp parallel for private(i)
+    for (i = 0; i < N; ++i) {
+      for (int j = 0; j < N; ++j) {
+        if(j < i){
+          buffer[i].updateForce(buffer[j]);
         }
-        
-        buffer[i].update(Nx, Ny);
-      
+        else if (i > j) {
+          buffer[i].calculateForce(particles[j]);
+        }
       }
-    
-    particles = buffer;
-    cout << "timestep :" << t << endl;
-
-    //printParticleVector2D(particles, N, Nx, Ny);
-
-    // sleep to see movement happen
-    //this_thread::sleep_for(std::chrono::milliseconds(500));
+      buffer[i].update(Nx, Ny);
     }
-  
+    particles = buffer;
+
+    if (print) {
+      cout << "timestep :" << t << endl;
+      printParticleVector2D(particles, N, Nx, Ny);
+      this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+  }
   // time measurement
   auto end_time = chrono::high_resolution_clock::now();
   auto duration = chrono::duration<double>(end_time - start_time).count();
