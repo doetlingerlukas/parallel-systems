75c1a7ffee8d3773e3108b93916352f2713f4c4c
Andreas Peintner <a.peintner@gmx.net>
2019-11-28 11:31:54 +0100

    fix makefile

--- assignment08/pi_omp.cpp
+++ assignment08/pi_omp.cpp
@@ -2,12 +2,28 @@
 #include <ctime>
 #include <cstdlib>
 #include <chrono>
+#include <time.h>
+#include <thread>
 
 using namespace std;
 
+long samplesInsideCircle(const int chunkSize)
+{
+    long count = 0;
+    for (auto i=0; i <= chunkSize; i++){
+        double x = ((double) rand() / RAND_MAX);
+        double y = ((double) rand() / RAND_MAX);
+        if ((x * x + y * y) <= 1) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
 int main(int argc, char **argv) {
 
-    auto samples = 100000000;
+    auto samples = 10000000;
 	if (argc > 1) {
         samples = strtol(argv[1], NULL, 10);
     }
@@ -16,17 +32,18 @@ int main(int argc, char **argv) {
 
     srand(time(NULL)); 
 
+    int numChunks = 8;
+    int chunk = samples / numChunks;
     long count = 0;
-    #pragma omp parallel for
-    for (auto i=0; i <= samples; i++){
-        double x = ((double) rand() / RAND_MAX);
-        double y = ((double) rand() / RAND_MAX);
-        if ((x * x + y * y) <= 1) {
-            count++;
-        }
+
+    #pragma omp parallel for shared(numChunks, chunk) reduction(+:count)
+    for (int i = 0; i < numChunks; i++)
+    {
+        count += samplesInsideCircle(chunk);
     }
 
     double pi = (4.0 * count) / samples; 
+
 
     auto end_time = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count();
@@ -37,3 +54,6 @@ int main(int argc, char **argv) {
 
 	return EXIT_SUCCESS;
 }
+
+
+// http://jakascorner.com/blog/2016/05/omp-monte-carlo-pi.html
