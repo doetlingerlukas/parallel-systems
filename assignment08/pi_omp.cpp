60a47bc353a3bf85574206ad56ab01fd0228a3e1
Andreas Peintner <a.peintner@gmx.net>
2019-11-28 10:54:22 +0100

    init and simple pi

---
+++ assignment08/pi_omp.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <chrono>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    auto samples = 100000000;
+	if (argc > 1) {
+        samples = strtol(argv[1], NULL, 10);
+    }
+
+    auto start_time = chrono::high_resolution_clock::now();
+
+    srand(time(NULL)); 
+
+    long count = 0;
+    #pragma omp parallel for
+    for (auto i=0; i <= samples; i++){
+        double x = ((double) rand() / RAND_MAX);
+        double y = ((double) rand() / RAND_MAX);
+        if ((x * x + y * y) <= 1) {
+            count++;
+        }
+    }
+
+    double pi = (4.0 * count) / samples; 
+
+    auto end_time = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count();
+    
+    cout << "\rπ ≈ " << pi << endl;
+    cout << "was calculated with " << samples << " samples!" << endl;
+    cout << "This took " << duration << " millisenconds." << endl;
+
+	return EXIT_SUCCESS;
+}
