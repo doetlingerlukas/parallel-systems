205c66293b52030b2a4fca31ee42e1627b0c4a39
moejoe95 <j.gugglberger@gmail.com>
2019-11-30 10:33:44 +0100

    add job script

---
+++ assignment08/job.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+while [[ $# -gt 0 ]]
+do 
+  key="$1"
+  case $key in 
+    -n|--mpi-n)
+      processes="$2"
+      shift
+      shift
+      ;;
+    -p|--program)
+      program="$2"
+      shift
+      shift
+      ;;
+  esac
+done
+
+#$ -q std.q
+#$ -cwd
+#$ -N assignment8
+#$ -o assignment8.dat
+#$ -j yes
+
+module load gcc/8.2.0
+
+export OMP_NUM_THREADS=$processes
+./$program
