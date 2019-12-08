b032ce2f65327de354ab4987e56566c35e031817
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2019-12-08 10:16:40 +0100

    Refactor input arguments.

--- assignment09/n_queens_omp.cpp
+++ assignment09/n_queens_omp.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int count = 0;
 
 void printSolution(int n, vector<int> hist){
-        printf("\nNo. %d\n-----\n", ++count);
+        printf("\nNo. %d\n-----\n", count++);
 		for (int i = 0; i < n; i++, putchar('\n')){
 			for (int j = 0; j < n; j++){
                 j == hist[i] ? cout << "Q|" :  cout << "_|";
@@ -30,13 +30,13 @@ int getFirstSavePos(int row, int col, vector<int> hist){
 void solve(int n, int col, vector<int> hist)
 {
 	if (col == n) { // if last column reached print solution and return
-		/*#pragma omp critical
-        printSolution(n, hist);*/
+		//#pragma omp critical
+      //printSolution(n, hist);
 		return;
 	}
     
 	for (int i = 0; i < n; i++) {
-        int j = getFirstSavePos(i, col, hist); // get save position of queen in given row i
+    int j = getFirstSavePos(i, col, hist); // get save position of queen in given row i
 
 		if (j < col) continue; // only results after col needs to be calculated
  
@@ -46,12 +46,20 @@ void solve(int n, int col, vector<int> hist)
 		solve(n, col + 1, hist); //recursive call with next column
 	}
 }
- 
-int main(int n, char **argv)
-{
+
+int main(int argc, char **argv) {
 	auto start_time = chrono::high_resolution_clock::now();
 
-	if (n <= 1 || (n = atoi(argv[1])) <= 0) n = 13;
+	unsigned int n = 13;
+	bool print = false;
+	
+	if (argc > 1) {
+    n = strtol(argv[1], nullptr, 10);
+    if (argc > 2) {
+      string verbose = argv[2];
+      print = verbose == "--verbose";
+    }
+  }
 
 	vector<int> hist(n, 0);
 
