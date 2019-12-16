2c93aa9a7e4a2ab2eee2356630eefe415172a7dc
pintonos <a.peintner@gmx.net>
2019-12-16 11:40:28 +0100

    add job script

---
+++ assignment10/job.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+while [[ $# -gt 0 ]]
+do 
+  key="$1"
+  case $key in 
+    -n|--mpi-n)
+      processes="$2"
+      shift
+      shift
+      ;;
+    -p|--program)
+      program="$2"
+      shift
+      shift
+      ;;
+  esac
+done
+
+#$ -q std.q
+#$ -cwd
+#$ -N heat_stencil_2D_hybrid
+#$ -o hybrid.dat
+#$ -j yes
+
+module load gcc/8.2.0
+module load openmpi/4.0.1
+
+export OMP_NUM_THREADS=$processes
+
+mpiexec -n $processes --mca btl self,vader,tcp $program
