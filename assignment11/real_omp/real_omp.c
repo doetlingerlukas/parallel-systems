1b48384b584de4c4484fbd382b041f232c5d3204
moejoe95 <j.gugglberger@gmail.com>
2020-01-13 18:55:28 +0100

    parallelize psinv

--- assignment11/real_omp/real_omp.c
+++ assignment11/real_omp/real_omp.c
@@ -464,11 +464,14 @@ static void psinv(void *or, void *ou, int n1, int n2, int n3,
   double (*r)[n2][n1] = (double (*)[n2][n1])or;
   double (*u)[n2][n1] = (double (*)[n2][n1])ou;
 
+  #pragma omp parallel
+  {
   int i3, i2, i1;
 
   double r1[M], r2[M];
 
   if (timeron) timer_start(T_psinv);
+  #pragma omp for collapse(2)
   for (i3 = 1; i3 < n3-1; i3++) {
     for (i2 = 1; i2 < n2-1; i2++) {
       for (i1 = 0; i1 < n1; i1++) {
@@ -492,6 +495,7 @@ static void psinv(void *or, void *ou, int n1, int n2, int n3,
     }
   }
   if (timeron) timer_stop(T_psinv);
+  }
 
   //---------------------------------------------------------------------
   // exchange boundary points
@@ -533,7 +537,7 @@ static void resid(void *ou, void *ov, void *or, int n1, int n2, int n3,
   double u1[M], u2[M];
 
   if (timeron) timer_start(T_resid);
-  #pragma omp for
+  #pragma omp for collapse(2)
   for (i3 = 1; i3 < n3-1; i3++) {
     for (i2 = 1; i2 < n2-1; i2++) {
       
@@ -802,7 +806,7 @@ static void interp(void *oz, int mm1, int mm2, int mm3,
     }
   }
   if (timeron) timer_stop(T_interp);
-
+  
   if (debug_vec[0] >= 1) {
     rep_nrm(z, mm1, mm2, mm3, "z: inter", k-1);
     rep_nrm(u, n1, n2, n3, "u: inter", k);
@@ -812,6 +816,7 @@ static void interp(void *oz, int mm1, int mm2, int mm3,
     showall(z, mm1, mm2, mm3);
     showall(u, n1, n2, n3);
   }
+  
 }
 
 
